6be649db91bb896f92dcacd30931be6457bd32f6
sagipenta <sagipenta@gmail.com>
2018-10-10 15:47:00 +0900

    ファイルコピーしてくるバッチ

---
+++ CopyFiles.bat
@@ -0,0 +1,6 @@
+echo on
+copy /y \\tres-pkg\tres-pkg\pkg\TresJ_Test\PS4\revision.txt C:\Users\saishige\Desktop\PS4\revision.txt
+copy /y \\tres-pkg\tres-pkg\pkg\TresJ_Test\PS4\JP0082-CUSA11060_00-KINGDOMHEARTSX30-A0100-V0100.pkg.for_disc0.gp4 C:\Users\saishige\Desktop\PS4\JP0082-CUSA11060_00-KINGDOMHEARTSX30-A0100-V0100.pkg.for_disc0.gp4
+copy /y \\tres-pkg\tres-pkg\pkg\TresJ_Test\PS4\JP0082-CUSA11060_00-KINGDOMHEARTSX30-A0100-V0100.pkg C:\Users\saishige\Desktop\PS4\JP0082-CUSA11060_00-KINGDOMHEARTSX30-A0100-V0100.pkg
+
+pause
