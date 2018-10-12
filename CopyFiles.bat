7d7625ef36970b2bfe6847e49a5a9d45a1a71506
sagipenta <sagipenta@gmail.com>
2018-10-12 11:40:36 +0900

    copy →　robocopy

--- CopyFiles.bat
+++ CopyFiles.bat
@@ -1,6 +1,8 @@
 echo on
-copy /y \\tres-pkg\tres-pkg\pkg\TresJ_Test\PS4\revision.txt C:\Users\saishige\Desktop\PS4\revision.txt
-copy /y \\tres-pkg\tres-pkg\pkg\TresJ_Test\PS4\JP0082-CUSA11060_00-KINGDOMHEARTSX30-A0100-V0100.pkg.for_disc0.gp4 C:\Users\saishige\Desktop\PS4\JP0082-CUSA11060_00-KINGDOMHEARTSX30-A0100-V0100.pkg.for_disc0.gp4
-copy /y \\tres-pkg\tres-pkg\pkg\TresJ_Test\PS4\JP0082-CUSA11060_00-KINGDOMHEARTSX30-A0100-V0100.pkg C:\Users\saishige\Desktop\PS4\JP0082-CUSA11060_00-KINGDOMHEARTSX30-A0100-V0100.pkg
+robocopy \\tres-pkg\tres-pkg\pkg\TresJ_Test\PS4\ C:\Users\saishige\Desktop\PS4 *.txt *.gp4 *.pkg
+
+::copy /y \\tres-pkg\tres-pkg\pkg\TresJ_Test\PS4\revision.txt C:\Users\saishige\Desktop\PS4\revision.txt
+::copy /y \\tres-pkg\tres-pkg\pkg\TresJ_Test\PS4\JP0082-CUSA11060_00-KINGDOMHEARTSX30-A0100-V0001.pkg.for_disc0.gp4 C:\Users\saishige\Desktop\PS4\JP0082-CUSA11060_00-KINGDOMHEARTSX30-A0100-V0001.pkg.for_disc0.gp4
+::copy /y \\tres-pkg\tres-pkg\pkg\TresJ_Test\PS4\JP0082-CUSA11060_00-KINGDOMHEARTSX30-A0100-V0001.pkg C:\Users\saishige\Desktop\PS4\JP0082-CUSA11060_00-KINGDOMHEARTSX30-A0100-V0001.pkg
 
 pause
